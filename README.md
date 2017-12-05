07f645f1ea28f0d969c7e93a375f326948203d66
Mehmet Burak Kurutmaz <burak.kurutmaz@boun.edu.tr>
2017-12-05 18:12:23 +0300

    Create website

--- README.md
+++
@@ -1,1 +0,0 @@
-# Academic Web Page
--- dist/js/main.js
+++
@@ -1,5 +0,0 @@
-$(document.body).ready(function() {
-	var contentHeight = document.getElementById('content').clientHeight;
-	$('#navigation').css("height",contentHeight);
-
-});
